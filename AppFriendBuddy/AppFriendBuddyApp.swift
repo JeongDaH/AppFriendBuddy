7e0dfb471e5b5828dec3a423deec52e9fbf1a8db
JeongDaH <hyejjang120300@gmail.com>
2023-05-04 09:52:58 +0900

    Initial Commit

---
+++ AppFriendBuddy/AppFriendBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppFriendBuddyApp.swift
+//  AppFriendBuddy
+//
+//  Created by dahye on 2023/05/04.
+//
+
+import SwiftUI
+
+@main
+struct AppFriendBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
